4cddea430fe9caffca54505f850a3747aa5e8dc4
AbdedjalilKaci <a_kaci@estin.dz>
2025-12-06 15:02:46 +0000

    add services and populars books sections

--- dz-kitab-frontend/src/pages/Landing-Page.jsx
+++ dz-kitab-frontend/src/pages/Landing-Page.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Hero } from '../components/landingpage-sections/hero'
 import { Services } from '../components/landingpage-sections/services'
+import { Books } from '../components/landingpage-sections/books'
 export const Landingpage = () => {
     return (
         <div className="">
             <Hero/> 
             <Services/>
+            <Books/>
         </div>
     )
 }
